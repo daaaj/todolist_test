d188d326ff7a0a5499e89dae5675f827f79b8390
dajeong <daaa_j@naver.com>
2023-02-09 10:58:45 +0900

    todolist 완성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todoList, setTodoList] = useState([
+        {
+            id: 1,
+            todo: 'react를 배워봅시다',
+        },
+    ]);
+    const [todo, setTodo] = useState('');
+
+    // input에 작성시
+    const writeTodo = (e) => {
+        setTodo(e.target.value);
+    };
+    // 추가하기 클릭시
+    const addTodo = () => {
+        if (todo !== '') {
+            const newTodo = {
+                id: Date.now(),
+                todo,
+            };
+            setTodoList([...todoList, newTodo]);
+            setTodo('');
+        }
+    };
+    return (
+        <div className="App">
+            <div className="header">
+                <input value={todo} onChange={writeTodo} />
+                <button onClick={addTodo}>추가하기</button>
+            </div>
+            <span>Todo List</span>
+            <div className="boxArea">
+                {todoList.map((list) => (
+                    <div className="listBox">{list.todo}</div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
